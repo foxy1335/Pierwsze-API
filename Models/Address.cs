12aa57fa10a9be20483e7b29559b06231b7645b0
Konrad Milewski <konradmilewski2000@interia.eu>
2021-03-13 17:25:01 +0100

    Dodaj pliki projektów.

---
+++ Models/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pierwsze_API.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulica")]
+        [StringLength(60, MinimumLength =3), Required(ErrorMessage = "To pole jest wymagane")]
+        public string Street { get; set; }
+        
+        [Display(Name = "Kod pocztowy")]
+        [Required(ErrorMessage = "Pole Kod pocztowy obowiązkowe")]
+        public string ZipCode { get; set; }
+     
+        [Display(Name = "Miasto")]
+        [Required(ErrorMessage = "Pole Miasto obowiązkowe")]
+        public string City { get; set; }
+
+        [Display(Name = "Numer")]
+        [Required]
+        public int Number { get; set; }
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pierwsze_API.Models;
+
+
+namespace Pierwsze_API.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            { return Page(); }
+
+            return RedirectToPage("./Privacy");
+        }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if(string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+
+        }
+    }
+}
